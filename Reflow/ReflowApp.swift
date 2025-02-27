a2b28d33696f3a71e205c109619c6b08945e0d5a
Sean04111 <2271448752@qq.com>
2025-02-27 13:58:29 +0800

    Initial Commit

---
+++ Reflow/ReflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflowApp.swift
+//  Reflow
+//
+//  Created by Sean on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ReflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReflowTests/ReflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReflowTests.swift
+//  ReflowTests
+//
+//  Created by Sean on 2/27/25.
+//
+
+import Testing
+@testable import Reflow
+
+struct ReflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
